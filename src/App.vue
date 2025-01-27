fe5a22f9df02c6cb8bcc83421cac23faed389e95
Finosyval <syldcomuser@gmail.com>
2025-01-27 23:10:19 +0100

    Version 1.0

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,102 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<form action="" @submit.prevent="addTodo"  >
+
+    <fieldset role="group">
+<input 
+v-model="newtodo"
+type="text"
+placeholder="La tâche à effectuer"
+>
+<button 
+
+:class="{'disabled-btn':newtodo.length===0}"
+> Ajouter une tâche</button>
+
+</fieldset>
+
+</form>
+
+<div v-if="todos.length==0" > 
+Mhhh, aucune tâche à effectuer pour le moment
+</div>
+<div v-else="">
+<ul>
+
+<li 
+v-for="todo in sortedTodos()" 
+:key="todo.date"
+:class="{'completed':todo.completed}">
+
+<input type="checkbox" v-model="todo.completed" >    
+{{ todo.title }}
+</li>
+</ul>
+</div>
+
+<input type="checkbox"
+v-model:="hideCompleted"
+>
+Masquer les tâches complétées
+
+<div>
+
+</div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref } from 'vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+
+const newtodo=ref('');
+const hideCompleted=ref(false);
+const todos=ref( [
+    {
+        title:'Apprendre Vue 3',
+        completed:true,
+        date:Date.now()
+    },
+    {
+        title:'Apprendre Vue Router',
+        completed:false,
+        date:Date.now()
+    }
+ 
+ 
+]);
+
+const addTodo=()=>{
+    todos.value.push({
+        title:newtodo.value,
+        completed:false,
+        date:Date.now()
+    });
+   newtodo.value='';
 }
+
+const sortedTodos= () => {
+    const AFiltrer =todos.value.toSorted((a,b)=>a.completed > b.completed ? 1 : -1);
+if (hideCompleted.value){
+    return AFiltrer.filter(todo=>!todo.completed);
+
+}
+
+return AFiltrer;
+}
+
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.disabled-btn {
+  cursor: not-allowed !important;
+  opacity: 0.6; /* Facultatif pour rendre visuellement désactivé */
 }
+
+
+.completed{
+    opacity: .5;
+    text-decoration: line-through;
+}
+
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false, // Désactive ESLint au démarrage
+  configureWebpack: {
+    devServer: {
+      client: {
+        overlay: {
+          warnings: false, // Désactive l'affichage des warnings dans le navigateur
+          errors: false,   // Désactive l'affichage des erreurs dans le navigateur
+        },
+      },
+    },
+  },
 })
